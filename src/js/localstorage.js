f17b4d2e99cbd5d003aa0d4c3fa4f879551d3f33
Pavel Sakun <axelhomkin@gmail.com>
2021-01-27 11:44:16 +0200

    all done, hope Im right

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -3,34 +3,62 @@ const Theme = {
   LIGHT: "light-theme",
   DARK: "dark-theme",
 };
+
 const refs = {
   toolBar: document.querySelector(".toolbar"),
   switch: document.querySelector("#theme-switch-toggle"),
   body: document.body,
 };
-let savedDark;
+
+const savedDark = localStorage.getItem("theme");
+if (savedDark === "dark-theme") {
+  refs.switch.checked = true;
+  refs.body.classList.add("dark-theme");
+}
+
 refs.body.classList.add("light-theme");
 refs.toolBar.addEventListener("change", handleSwitchTheme);
 
 function handleSwitchTheme(event) {
-  savedDark = localStorage.getItem("theme", Theme.DARK);
-  switch (event.target.checked) {
-    case true:
-    //   console.log("object", localStorage.getItem("theme") === Theme.DARK);
-    case savedDark === Theme.DARK:
-      refs.body.classList.remove("light-theme");
-      refs.body.classList.add("dark-theme");
-      localStorage.setItem("theme", Theme.DARK);
-      break;
-
-    default:
-      refs.body.classList.remove("dark-theme");
-      refs.body.classList.add("light-theme");
-      localStorage.removeItem("theme");
-      localStorage.setItem("theme", Theme.LIGHT);
-      break;
+  event.preventDefault();
+  if (!event.target.checked) {
+    refs.body.classList.remove("dark-theme");
+    refs.body.classList.add("light-theme");
+    localStorage.removeItem("theme");
+    localStorage.setItem("theme", Theme.LIGHT);
+  } else {
+    refs.body.classList.remove("light-theme");
+    refs.body.classList.add("dark-theme");
+    localStorage.setItem("theme", Theme.DARK);
   }
 }
+
+// switch (event.target.checked) {
+//   case true:
+//     //   console.log("object", localStorage.getItem("theme") === Theme.DARK);
+//     // case savedDark === Theme.DARK:
+//     refs.body.classList.remove("light-theme");
+//     refs.body.classList.add("dark-theme");
+//     localStorage.setItem("theme", Theme.DARK);
+//     break;
+//   case refs.switch.checked === (savedDark === "dark-theme"):
+//     console.log(
+//       "my try",
+//       refs.switch.checked === (savedDark === "dark-theme"),
+//     );
+//     refs.body.classList.remove("light-theme");
+//     refs.body.classList.add("dark-theme");
+//     localStorage.setItem("theme", Theme.DARK);
+//     break;
+
+//   default:
+//     refs.body.classList.remove("dark-theme");
+//     refs.body.classList.add("light-theme");
+//     localStorage.removeItem("theme");
+//     localStorage.setItem("theme", Theme.LIGHT);
+//     break;
+// }
+// }
 // const savedDark = localStorage.getItem("theme", Theme.DARK);
 // console.log("savedDark", !savedDark);
 // if (!savedDark) {
