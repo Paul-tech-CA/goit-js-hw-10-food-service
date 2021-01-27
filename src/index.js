ed26f41d63f9c85c164b030be854533196d6e24f
Pavel Sakun <axelhomkin@gmail.com>
2021-01-27 04:13:31 +0200

    tryed made switcher

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import ingredientsData from "./data-base/menu.json";
 import dishTemplate from "./templates/dish.hbs";
+import "./js/localstorage.js";
 
 import "./css/style.css";
 
 const markup = dishTemplate(ingredientsData);
 document.querySelector(".js-menu").insertAdjacentHTML("afterbegin", markup);
-
-console.log(ingredients);
---
+++ src/js/localstorage.js
@@ -0,0 +1,66 @@
+import "../css/style.css";
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+const refs = {
+  toolBar: document.querySelector(".toolbar"),
+  switch: document.querySelector("#theme-switch-toggle"),
+  body: document.body,
+};
+let savedDark;
+refs.body.classList.add("light-theme");
+refs.toolBar.addEventListener("change", handleSwitchTheme);
+
+function handleSwitchTheme(event) {
+  savedDark = localStorage.getItem("theme", Theme.DARK);
+  switch (event.target.checked) {
+    case true:
+    //   console.log("object", localStorage.getItem("theme") === Theme.DARK);
+    case savedDark === Theme.DARK:
+      refs.body.classList.remove("light-theme");
+      refs.body.classList.add("dark-theme");
+      localStorage.setItem("theme", Theme.DARK);
+      break;
+
+    default:
+      refs.body.classList.remove("dark-theme");
+      refs.body.classList.add("light-theme");
+      localStorage.removeItem("theme");
+      localStorage.setItem("theme", Theme.LIGHT);
+      break;
+  }
+}
+// const savedDark = localStorage.getItem("theme", Theme.DARK);
+// console.log("savedDark", !savedDark);
+// if (!savedDark) {
+//   refs.body.classList.replace("dark-theme", "light-theme");
+//   localStorage.removeItem("theme");
+//   localStorage.setItem("theme", Theme.LIGHT);
+
+// else {
+//   refs.switch.checked = true;
+//   document.body.classList.add("dark-theme");
+// }
+// function handleSwitchTheme(event) {
+//   event.preventDefault();
+//   console.log(event.target.checked);
+//   const checked = event.target.checked;
+//   if (checked) {
+//     document.body.classList.add("dark-theme");
+//     localStorage.setItem("theme", Theme.DARK);
+//   } else {
+//     document.body.classList.replace("dark-theme", "light-theme");
+//     localStorage.removeItem("theme");
+//     localStorage.setItem("theme", Theme.LIGHT);
+//   }
+// }
+// const savedDark = localStorage.getItem("theme", Theme.DARK);
+// if (savedDark) document.body.classList.add("dark-theme");
+//   document.body.classList.add("dark-theme");
+// } else {
+//   document.body.classList.replace("dark-theme", "light-theme");
+//   localStorage.removeItem("theme");
+//   localStorage.setItem("theme", Theme.LIGHT);
+
+// console.log("theme!", savedDark);
